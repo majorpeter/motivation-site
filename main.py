a62b7017e388e0311262a50a80cd88fc6d3c307a
Major Péter <majorpeter29@gmail.com>
2019-08-20 21:05:32 +0200

    added workout days/weight chart

--- main.py
+++ main.py
@@ -10,13 +10,26 @@ app = Flask(__name__)
 def index_page():
     days_since_workout_message, days_since_workout_icon = workout.days_since_workout_message_html()
     workout_cal_header, workout_cal = workout.workout_calendar()
+    workout_days_data = workout.get_workout_days_for_range()
+
+    workout_dates = [x['date'] for x in workout_days_data]
+    weight_measurements = [x['weight'] if 'weight' in x else None for x in workout_days_data]
+    workouts_per_week = [x['count'] for x in workout_days_data]
+
+    workout_dates.reverse()
+    weight_measurements.reverse()
+    workouts_per_week.reverse()
+
     data = {
         'quote': quotes.get_random_quote()[:-1].replace('\n', '<br/>'),
         'days_since_workout': days_since_workout_message,
         'days_since_workout_fas_icon': days_since_workout_icon,
         'workout_cal_header': workout_cal_header,
         'workout_cal': workout_cal,
-        'workout': workout.get_workout_days_for_range()
+        'workout_dates': workout_dates,
+        'workouts_per_week': workouts_per_week,
+        'weight_measurements': weight_measurements,
+        'workout': workout_days_data,
     }
     return render_template('index.html', **data)
 
