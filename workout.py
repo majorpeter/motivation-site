d051c1ea497a37a8b4d2c8cdd3c98f52aa4c6d24
Major Péter <majorpeter29@gmail.com>
2019-08-20 15:16:03 +0200

    fixed counting and text formatting errors in workout.py

--- workout.py
+++ workout.py
@@ -33,7 +33,7 @@ def get_workout_days_for_range(days_range=7):
 
     while i >= 0:
         count = 0
-        for j in range(i - 1, -1, -1):
+        for j in range(i, -1, -1):
             day_earlier = time.strptime(entries[j][0], config['date_format'])
             delta_sec = time.mktime(date) - time.mktime(day_earlier)
             delta_day = int(delta_sec / 24 / 3600)
@@ -42,7 +42,6 @@ def get_workout_days_for_range(days_range=7):
             count += 1
 
         if entries[i][0] == date_str:
-            count += 1
             data = {'date': date_str, 'count': count, 'workout_day': True, 'weight': float(entries[i][1])}
             i -= 1
         else:
@@ -70,6 +69,6 @@ def days_since_workout_message_html():
     elif days == 1:
         return 'You have worked out <strong>yesterday</strong>.', 'fa-thumbs-up'
     elif days < 4:
-        return 'You have worked out <strong>%d days ago</strong>, maybe it\'s time to hit the gym?', 'fa-exclamation-triangle'
+        return 'You have worked out <strong>%d days ago</strong>, maybe it\'s time to hit the gym?' % days, 'fa-exclamation-triangle'
     else:
-        return 'You haven\'t worked out in <strong>%d days</strong>! Go training now!', 'fa-exclamation'
+        return 'You haven\'t worked out in <strong>%d days</strong>! Go training now!' % days, 'fa-exclamation'
