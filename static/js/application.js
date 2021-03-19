00672bd1a18b681228e302e891a128b6d3c8612e
Major Péter <majorpeter29@gmail.com>
2021-03-19 23:03:27 +0100

    periodically update random closed issue with fade in/out

--- static/js/application.js
+++ static/js/application.js
@@ -1,7 +1,25 @@
+function periodicallyReload(e) {
+    $(e).fadeOut(800, function() {
+        $.get($(e).data('url'), function(data) {
+            $(e).html(data);
+            $(e).fadeIn(400, function() {
+                setTimeout(function() {
+                    periodicallyReload(e);  // reschedule self
+                }, $(e).data('period'));
+            });
+        });
+    });
+}
+
 $(document).ready(function() {
     $('div.lazy-load').each(function(i, e) {
         $.get($(e).data('url'), function(data) {
             $(e).html(data);
         });
+        if ($(e).data('period')) {
+            setTimeout(function() {
+                periodicallyReload(e);
+            }, $(e).data('period'));
+        }
     });
 });
